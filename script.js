f81f20afe49561953a5342178a0e7d4dd60eac22
Your Name <elgunmemmedli95@gmail.com>
2020-03-06 08:33:49 -0800

    Travelly

---
+++ script.js
@@ -0,0 +1,12 @@
+let take = document.getElementById("navbars");
+document.getElementById("hid").style.display="none";
+function Show(){
+  take.style.display="block";
+  document.getElementById("bar").style.display="none";
+  document.getElementById("hid").style.display="block";
+}
+function Hide(){
+    take.style.display="none";
+    document.getElementById("bar").style.display="block";
+  document.getElementById("hid").style.display="none";
+}
